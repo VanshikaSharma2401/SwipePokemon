d7f2e52f1bfa7e256f42e0655f23d7e6230643f5
VanshikaSharma2401 <bulbulsharma2401@gmail.com>
2024-05-29 03:11:11 +0530

    fixed welcome page

--- src/Components/WelcomeScreen.jsx
+++ src/Components/WelcomeScreen.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 //initial welcome screen
 const WelcomeScreen = ({ onStart }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-4">Welcome to PokéSwipe!</h1>
-      <p className="mb-8">Swipe through Pokémon and build your dream team!</p>
+      <h5 className=" font-bold mb-4">How to play PokeSwipe</h5>
+      <p className="mb-8">Pokemon Appear One at a Time choose "Like" or "Dislike" Build Your Favourite Team!</p>
       <button 
         className="px-4 py-2 bg-blue-500 text-white rounded-lg"
         onClick={onStart}
       >
-        Start
+        Let's Go!
       </button>
     </div>
   );
